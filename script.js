7bafd30d249948415dde478cd323b2cad750ac80
GitJulle <105446170+GitJulle@users.noreply.github.com>
2023-09-15 10:47:59 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,4 +3,7 @@ console.log("Up and running!")
 const Operia = {addel: function(number) {
   console.log("The number is " + number)
   alert(number + " Potatoes")
+}, lev: function(base, height) {
+  console.log("Alright!")
+  document.getElementById("lev").innerText = "I am groot " + base * height
 }}

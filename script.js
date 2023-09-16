9a82bae61e31cba3556a4305be534a95948ccfb5
GitJulle <105446170+GitJulle@users.noreply.github.com>
2023-09-16 09:22:28 +0200

    Update script.js

--- script.js
+++ script.js
@@ -6,4 +6,17 @@ const Operia = {addel: function(number) {
 }, lev: function(base, height) {
   console.log("Alright!")
   document.getElementById("lev").innerText = "I am groot " + base * height
+}, cookies: 0, cookie: function(attribute) {
+  console.log("The cookie gods have listened")
+  if (attribute == "clumb") {
+    console.log("Cookie gods: We have gotten your request!")
+    alert("Cookie gods: We will do what you want.")
+    Operia.cookies += 100
+  }
+  else if (attribute == "Capybara") {
+    Operia.cookies += 10000
+  }
+  else {
+    Operia.cookies = 0
+  }
 }}
